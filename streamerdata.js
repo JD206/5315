c1a90d9c204d6454c6db12696402986d973f26b1
Jeff <dickson1@seattleu.edu>
2018-10-12 16:10:32 -0700

    HW2 Files Revised Final

---
+++ streamerdata.js
@@ -0,0 +1,123 @@
+{
+  "data": [
+ {
+   "Streamer_Name": "Bizarre Light",
+   "Twitch_ID": "Bizarre99",
+   "Favorite_Genre": "Racing",
+   "Favorite_Game": "Forza Horizon 4",
+   "Annual_Tips": "$14,500 ",
+   "Preferred_Platform": "PC"
+ },
+ {
+   "Streamer_Name": "Erik J",
+   "Twitch_ID": "UW_Straif",
+   "Favorite_Genre": "Platformer",
+   "Favorite_Game": "Duck Game",
+   "Annual_Tips": "$20,000 ",
+   "Preferred_Platform": "Xbox One"
+ },
+ {
+   "Streamer_Name": "Kit boga",
+   "Twitch_ID": "Kitboga",
+   "Favorite_Genre": "Scambait",
+   "Favorite_Game": "Scambaiting",
+   "Annual_Tips": "$84,500 ",
+   "Preferred_Platform": "PC"
+ },
+ {
+   "Streamer_Name": "Douglas F",
+   "Twitch_ID": "FPSDoug",
+   "Favorite_Genre": "FPS",
+   "Favorite_Game": "Counterstrike",
+   "Annual_Tips": "$1,337 ",
+   "Preferred_Platform": "PC"
+ },
+ {
+   "Streamer_Name": "Nate N",
+   "Twitch_ID": "Wild Skwerls",
+   "Favorite_Genre": "Racing",
+   "Favorite_Game": "Rocket League",
+   "Annual_Tips": "$5,000 ",
+   "Preferred_Platform": "Xbox One"
+ },
+ {
+   "Streamer_Name": "Andrew K",
+   "Twitch_ID": "KBBQ",
+   "Favorite_Genre": "FPS",
+   "Favorite_Game": "Ring of Elysium",
+   "Annual_Tips": "$18,000 ",
+   "Preferred_Platform": "Playstation 4"
+ },
+ {
+   "Streamer_Name": "Brandon Q",
+   "Twitch_ID": "MadHat29",
+   "Favorite_Genre": "Puzzle",
+   "Favorite_Game": "Myst",
+   "Annual_Tips": "$1,000 ",
+   "Preferred_Platform": "PC"
+ },
+ {
+   "Streamer_Name": "Colin Ranst",
+   "Twitch_ID": "Desifinado",
+   "Favorite_Genre": "Racing",
+   "Favorite_Game": "Colin Mcrae Rally",
+   "Annual_Tips": "$6,000 ",
+   "Preferred_Platform": "Xbox One"
+ },
+ {
+   "Streamer_Name": "Matthew P",
+   "Twitch_ID": "Dr. Disrespect",
+   "Favorite_Genre": "FPS",
+   "Favorite_Game": "PlayerUnknowns' Battlegrounds",
+   "Annual_Tips": "$420,000 ",
+   "Preferred_Platform": "PC"
+ },
+ {
+   "Streamer_Name": "Sanyo T",
+   "Twitch_ID": "Penguin34",
+   "Favorite_Genre": "Strategy",
+   "Favorite_Game": "Civilization V",
+   "Annual_Tips": "$23,000 ",
+   "Preferred_Platform": "PC"
+ },
+ {
+   "Streamer_Name": "Jackson P",
+   "Twitch_ID": "PainterCrazy",
+   "Favorite_Genre": "Platformer",
+   "Favorite_Game": "Super Mario Galaxy",
+   "Annual_Tips": "$82,000 ",
+   "Preferred_Platform": "Wii U"
+ },
+ {
+   "Streamer_Name": "Thomas N",
+   "Twitch_ID": "Sarif",
+   "Favorite_Genre": "FPS",
+   "Favorite_Game": "Doom",
+   "Annual_Tips": "$103,000 ",
+   "Preferred_Platform": "PC"
+ },
+ {
+   "Streamer_Name": "Johnny P",
+   "Twitch_ID": "FighterFoo",
+   "Favorite_Genre": "Music",
+   "Favorite_Game": "Rock Band 3",
+   "Annual_Tips": "$140,000 ",
+   "Preferred_Platform": "Xbox One"
+ },
+ {
+   "Streamer_Name": "Alton M",
+   "Twitch_ID": "Wojtek",
+   "Favorite_Genre": "Racing",
+   "Favorite_Game": "Need For Speed Underground",
+   "Annual_Tips": "$6,500 ",
+   "Preferred_Platform": "PC"
+ },
+ {
+   "Streamer_Name": "Anderson L",
+   "Twitch_ID": "Razor",
+   "Favorite_Genre": "Racing",
+   "Favorite_Game": "Need For Speed Most Wanted",
+   "Annual_Tips": "$55,432 ",
+   "Preferred_Platform": "Xbox 360"
+ }
+]}
